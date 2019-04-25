26983f408583b5ecc551eae76b657de2b42657ef
DeeptiShukla <deeptishukla24@gmail.com>
2019-04-24 23:24:42 -0500

    added validations

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -4,4 +4,36 @@ class AdminUser < ApplicationRecord
   has_many :sections, :through => :section_edits
 
   attr_accessor :first_name, :last_name
+
+  EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
+  FORBIDDEN_USERNAMES = ['temp','test','testuser']
+
+  validates :first_name, :presence => true,
+												 :length => { :maximum => 25 }
+  validates :last_name, :presence => true,
+                        :length => { :maximum => 50 }
+  validates :username, :length => { :within => 8..25 },
+                       :uniqueness => true
+  validates :email, :presence => true,
+                    :length => { :maximum => 100 },
+                    :format => EMAIL_REGEX,
+                    :confirmation => true
+
+  validate :username_is_allowed
+  validate :no_new_users_on_wednesday, :on => :create
+
+  private
+
+  def username_is_allowed
+    if FORBIDDEN_USERNAMES.include?(username)
+      errors.add(:username, "has been restricted from use.")
+    end
+  end
+
+  def no_new_users_on_wednesday
+    if Time.now.wday == 3
+      errors.add(:base, "No new users on Wednesdays.")
+    end
+  end
+
 end
